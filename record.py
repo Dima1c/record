6351db29dea95a1fef364705d75f8cd20ef132bb
Jonathan D. Lettvin <jlettvin@gmail.com>
2015-09-07 12:22:36 -0400

    pep8 and pylint cleaner

--- record.py
+++ record.py
@@ -26,7 +26,7 @@ phrase.hello.world.wav is generated.
 """
 
 from os import (kill)
-from sys import (stdout)
+from sys import (stdout, exc_info)
 from signal import (SIGTERM)
 from docopt import (docopt)
 from subprocess import (Popen, PIPE)
@@ -35,27 +35,36 @@ from Raw.Raw import (Raw)
 
 VERSION = "record.py 1.0.0"
 
+
 def crprint(msg):
+    "crprint outputs a same-line msg."
     print '\r%s\r%s\r' % (' '*79, msg),
     stdout.flush()
 
-def prompt(pre, s, post):
-    crprint('%s: "%s".  --Press any key to %s.--' % (pre, s, post))
+
+def prompt(pre, msg, post):
+    "prompt shows a message, listens for a raw key, then clears the message."
+    crprint('%s: "%s".  --Press any key to %s.--' % (pre, msg, post))
     with Raw() as raw:
-        while not raw.kbhit(): pass
+        while not raw.kbhit():
+            pass
         raw.getch()
     crprint('')
 
+
 def keep(source):
+    "keep converts a phrase to filename characters."
     target = ''
-    for c in source:
-        if c.isalnum() or c in ['_', '-']:
-            target += c
-        elif c == ' ':
+    for char in source:
+        if char.isalnum() or char in ['_', '-']:
+            target += char
+        elif char == ' ':
             target += '.'
     return target
 
+
 def record(phrase, **kw):
+    "record uses prompts and sox to collect a sound sample."
     prefix, ext = [kw.get('prefix', 'phrase'), kw.get('ext', 'wav')]
     filename = '%s.%s.%s' % (prefix, keep(phrase), ext)
     options = ['-c 2', '-r 16000 -b 16 -c 1']
@@ -66,23 +75,25 @@ def record(phrase, **kw):
         prompt('Recording', phrase, 'stop')
         kill(pid, SIGTERM)
     except:
-        pass
+        print 'Unexpected exception:', exc_info()[0]
 
 if __name__ == "__main__":
 
     def test():
-        phrases = ["hello world", "klatu barada nikto", "fubar" ]
+        "test is a simple unit-test."
+        phrases = ["hello world", "klatu barada nikto", "fubar"]
         print 'The following recordings are less than 2 minutes of speech.'
         for phrase in phrases:
             record(phrase)
 
     def main(**kw):
+        "The starting point of execution."
         if kw.get('--unit', False):
             test()
         else:
             for phrase in kw.get('<phrase>', []):
                 record(phrase)
 
-    kwargs = docopt(__doc__, version=VERSION)
+    KWARGS = docopt(__doc__, version=VERSION)
 
-    main(**kwargs)
+    main(**KWARGS)
